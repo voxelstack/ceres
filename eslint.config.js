833f38840da840517f0b0c02ae3669cbd49b7705
voxelstack <voxelstack@gmail.com>
2024-12-17 19:05:36 -0300

    Map store.

--- eslint.config.js
+++ eslint.config.js
@@ -7,5 +7,16 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {rules: { "@typescript-eslint/no-explicit-any": "off" }},
+  {rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{
+				argsIgnorePattern: "^_",
+				varsIgnorePattern: "^_",
+				caughtErrorsIgnorePattern: "^_"
+			}
+		]
+  }},
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
 import { render, createRenderable, format } from "./lib/render";
-import { AtomStore as AtomicStore, DerivedStore } from "./lib/store";
+import { AtomStore as AtomicStore, DerivedStore, MapStore } from "./lib/store";
 
 const en = new AtomicStore("Ceres・Fauna・");
 const jp = new AtomicStore("セレス・ファウナ・");
 const count = new AtomicStore(0);
-const upper = new DerivedStore([en], ([e]) => e.toUpperCase()); 
+const upper = new DerivedStore([en], ([e]) => e.toUpperCase());
 const double = new DerivedStore([count], ([c]) => 2 * c);
+const tubers = new MapStore<{ gen: { promise: {
+    name: string,
+    mark: string
+}}}>({ gen: { promise: {
+    name: "Ceres Fauna",
+    mark: "🌿"
+}}});
+
+tubers.subscribeKey("gen.promise.name", console.log);
+tubers.subscribeKey("gen", console.log);
+
+setTimeout(() =>
+    tubers.setKey("gen.promise.name", "Ceres Fauna (graduated)"),
+    1000
+);
+setTimeout(() =>
+    tubers.setKey("gen", { promise: { name: "LemonLeaf", mark: "🌙" }}),
+    2000
+);
 
 function scroll(str: string) {
     return `${str.slice(-1)}${str.slice(0, -1)}`;
