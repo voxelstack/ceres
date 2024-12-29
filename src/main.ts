07ea88cc42136f81c12b26504c52fdac04d37e74
voxelstack <voxelstack@gmail.com>
2024-12-29 02:35:36 -0300

    Export API types.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
-import "./index";
+export {
+    $transform
+} from "./lib/bind"
+
+export {
+    type $dyn,
+    $createComponent,
+    $element,
+    $fragment,
+    $boundary,
+    $head,
+    $window,
+    $document,
+    $body
+} from "./lib/component";
+
+export {
+    $if,
+    $each,
+    $key,
+    $await
+} from "./lib/directive";
+
+export {
+    $handler
+} from "./lib/event";
+
+export {
+    $format,
+    $text
+} from "./lib/reactive_string";
+
+export {
+    $store,
+    $derive,
+    $registry
+} from "./lib/store";
